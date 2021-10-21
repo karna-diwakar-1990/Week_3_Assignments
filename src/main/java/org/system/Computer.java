1ee158ee33002e32f23ef9ff4f3dae69c5b287da
karna diwakar <diwakarkarna1990@outlook.com>
2021-10-21 20:52:14 +0530

    Assignment 1

---
+++ src/main/java/org/system/Computer.java
@@ -0,0 +1,13 @@
+package org.system;
+
+/**
+ * Hello world!
+ *
+ */
+public class Computer
+{
+    void computerModel(){
+        System.out.println("Display computer model");
+    }
+
+}
---
+++ src/main/java/org/system/Desktop.java
@@ -0,0 +1,8 @@
+package org.system;
+
+public class Desktop extends Computer {
+    void desktopSize(){
+        System.out.println("Display desktop size");
+        super.computerModel();
+    }
+}
